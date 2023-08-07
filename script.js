b60a77d300d5395d61dcf6d67132627a1dfb9c52
Siriuszx <114768817+Siriuszx@users.noreply.github.com>
2023-08-07 19:20:21 +0200

    Complete submit logic and checkPass logic

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
+const signupForm = document.querySelector('#sign-up');
 const submitBtn = document.querySelector('.form-submit');
+
 const passContainer = document.querySelector('.pass-container');
 const passField = document.querySelector('#form-pass');
 const passConf = document.querySelector('#form-conf-pass');
-const signupForm = document.querySelector('#sign-up');
 
 const passMsg = document.createElement('div');
 
 // This is a soup. I should've separated this code logically. But instead
 // it doesn't just confirms if pass is correct.
-// It changes style of other elements. Validates form and controls
+// It changes style of other elements, validates form and controls
 // submit function of a form.
 // Break down code into smaller manageable pieces.
 
-function checkConfPass() { 
+//UPD: Much better I guess?
+
+function checkPass() {
     if (passField.value !== passConf.value) {
         passMsg.style.color = 'red';
         passMsg.textContent = '* Passwords do not match';
         passContainer.appendChild(passMsg);
 
+        passField.classList.remove('pass-correct');
+        passConf.classList.remove('pass-correct');
+
         passField.classList.add('pass-error');
         passConf.classList.add('pass-error');
-        
-    } else if (passField.value && passConf.value) {
+
+        return false;
+    } else if (passField.value === passConf.value && passField.value && passConf.value) {
         passMsg.style.color = 'lightgreen';
-        passMsg.textContent = 'Passwords Match ✓';
+        passMsg.textContent = 'Passwords match ✓';
         passContainer.appendChild(passMsg);
 
         passField.classList.remove('pass-error');
         passConf.classList.remove('pass-error');
-        
-        if(signupForm.reportValidity()) signupForm.requestSubmit();
-    } else {
-        signupForm.reportValidity();
+
+        passField.classList.add('pass-correct');
+        passConf.classList.add('pass-correct');
+
+        return true;
     }
 }
 
-submitBtn.addEventListener('click', checkConfPass);
+function submitForm() {
+    if (signupForm.reportValidity() && checkPass()) 
+        signupForm.requestSubmit();
+}
+
+passField.addEventListener('input', checkPass);
+passConf.addEventListener('input', checkPass);
+submitBtn.addEventListener('click', submitForm);
